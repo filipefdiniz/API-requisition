dbe85f681f53d3eb5f0534c776cd1e346d1cdae8
Filipe Ferreira <filipe.ferreira@sxgrupo.com>
2023-09-19 10:04:33 -0300

    Style tratativa

--- script.js
+++ script.js
@@ -1,98 +1,107 @@
 document.addEventListener('DOMContentLoaded', function () {
+	
+			var inputName = document.getElementById('form-field-name');
+			var valueName;
+			var inputUsername = document.getElementById('form-field-username_api');
+			var valueUsername;
+			var inputEmail = document.getElementById('form-field-email_api');
+			var valueEmail;
+			var inputTelefone = document.getElementById('form-field-telefone');
+			var valueTelefone;
+	
+			inputName.addEventListener('input', function() {
+						valueName = inputName.value;
+				});
+			inputUsername.addEventListener('input', function() {
+					valueUsername = inputUsername.value;
+				});
+	
+			inputEmail.addEventListener('input', function() {
+						valueEmail = inputEmail.value;
+				});
+			inputTelefone.addEventListener('input', function() {
+						valueTelefone = inputTelefone.value;
+				});
+	
+		
+        // Event listener para a submissão do formulário
+        document.getElementById('btn_api').addEventListener('click', function (e) {
+            e.preventDefault(); // Impede o envio padrão do formulário
+						console.log('CLICANDO BOTÃO', inputUsername.value);
 
-    var inputName = document.getElementById('form-field-name');
-    var valueName;
-    var inputUsername = document.getElementById('form-field-username_api');
-    var valueUsername;
-    var inputEmail = document.getElementById('form-field-email_api');
-    var valueEmail;
-    var inputTelefone = document.getElementById('form-field-telefone');
-    var valueTelefone;
+            // Formata os dados do formulário para o formato desejado
+            var new_data = {
+                username: valueUsername,
+                nickname: valueUsername,
+                birthdate: '2000-01-01',
+                club: 32442,
+                email: valueEmail
+            };
 
-    inputName.addEventListener('input', function () {
-        valueName = inputName.value;
-    });
-    inputUsername.addEventListener('input', function () {
-        valueUsername = inputUsername.value;
-    });
+            // Cria um objeto XMLHttpRequest
+            var xhr = new XMLHttpRequest();
+						
+            // Configura a solicitação POST para a API remota
+            xhr.open('POST', 'https://homologacao.sxgrupo.com.br/api/sx-suprema/register_player_slot', true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.setRequestHeader('Authorization', 'ubUjHPeaPXgdcWMz7luKfSF5xP49X3');
 
-    inputEmail.addEventListener('input', function () {
-        valueEmail = inputEmail.value;
-    });
-    inputTelefone.addEventListener('input', function () {
-        valueTelefone = inputTelefone.value;
-    });
-
-
-    // Event listener para a submissão do formulário
-    document.getElementById('btn_api').addEventListener('click', function (e) {
-        e.preventDefault(); // Impede o envio padrão do formulário
-        console.log('CLICANDO BOTÃO', inputUsername.value);
-
-        // Formata os dados do formulário para o formato desejado
-        var new_data = {
-            username: valueUsername,
-            nickname: valueUsername,
-            birthdate: '2000-01-01',
-            club: 32442,
-            email: valueEmail
-        };
-
-        // Cria um objeto XMLHttpRequest
-        var xhr = new XMLHttpRequest();
-
-        // Configura a solicitação POST para a API remota
-        xhr.open('POST', 'https://homologacao.sxgrupo.com.br/api/sx-suprema/register_player_slot', true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.setRequestHeader('Authorization', 'ubUjHPeaPXgdcWMz7luKfSF5xP49X3');
-
-        // Define a função de retorno de chamada para manipular a resposta da API
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                // Manipule a resposta da API, se necessário
-                var response = JSON.parse(xhr.responseText);
-                console.log(response);
-
-                //Caso email já exista
-                if (response.includes('email account exists')) {
-
-                    //Destacar campo email
-                    var inputError = document.getElementById('form-field-email_api');
-                    inputError.placeholder = 'Email já cadastrado';
-                    inputError.style.border = '2px solid red';
-                    inputError.style.background = 'rgb(255,0,0,0.05)';
-                    inputError.style.color = 'red';
-
-                    //Preencher os valores
-                    console.log(valueName, valueUsername, valueTelefone);
-                    inputName.value = valueName;
-                    inputUsername.value = valueUsername;
-                    inputTelefone.value = valueTelefone;
-
-                    //Mensagem de erro abaixo do formulário
-                    var errorMessage = document.getElementById('error_message');
-                    errorMessage.innerText = '*Email já cadastrado. Tente outro endereço de email para prosseguir.';
-
+            // Define a função de retorno de chamada para manipular a resposta da API
+            xhr.onload = function () {
+                if (xhr.status === 200) {
+                    // Manipule a resposta da API, se necessário
+                    var response = JSON.parse(xhr.responseText);
+										console.log(response);
+									
+									//Caso email já exista
+									if(response.includes('email account exists')){
+											
+											//Destacar campo email
+											var inputError = document.getElementById('form-field-email_api');
+											inputError.placeholder = 'Email já cadastrado';
+											inputError.style.border = '2px solid red';
+											inputError.style.background = 'rgb(255,0,0,0.05)';
+											inputError.style.color = 'red';
+		
+											//Preencher os valores
+											console.log(valueName, valueUsername, valueTelefone);
+											inputName.value = valueName;
+											inputUsername.value = valueUsername;
+											inputTelefone.value = valueTelefone;	
+											
+											//Mensagem de erro abaixo do formulário
+											var errorMessage = document.getElementById('error_message');
+											errorMessage.style.display = 'block';
+											
+											//Anular style da tratativa quando inserir um novo email
+											inputEmail.addEventListener('input', function() {
+													errorMessage.style.display = 'none';
+													inputError.placeholder = 'Insira o email';
+													inputError.style.border = 'none';
+													inputError.style.background = 'rgb(255,255,255)';
+													inputError.style.color = 'black';
+											});
+											
+										}
+									//caso email não exista
+									else{
+										document.location.href = 'https://sxpokers.com.br/generica-v1-api-tp/?preview_id=306&preview_nonce=8f5561b110&preview=true';
+									}
+								}
+	
+               else {
+                    // Trate erros
+                    console.alert('Erro' + response + '. Tente novamente.');
                 }
-                //caso email não exista
-                else {
-                    document.location.href = 'https://sxpokers.com.br/generica-v1-api-tp/?preview_id=306&preview_nonce=8f5561b110&preview=true';
-                }
-            }
+            };
 
-            else {
-                // Trate erros
-                console.alert('Erro' + response + '. Tente novamente.');
-            }
-        };
+            // Trata erros de rede
+            xhr.onerror = function () {
+                console.error('Erro de rede ao fazer a solicitação para a API.');
+            };
 
-        // Trata erros de rede
-        xhr.onerror = function () {
-            console.error('Erro de rede ao fazer a solicitação para a API.');
-        };
-
-        // Envia a solicitação com os dados do formulário no formato JSON
-        xhr.send(JSON.stringify(new_data));
+            // Envia a solicitação com os dados do formulário no formato JSON
+            xhr.send(JSON.stringify(new_data));
+        });
+	
     });
-
-});
